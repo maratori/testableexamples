1f9f8421d7883761e4f10d8593d0159ed5c0ea00
Marat Reymers <marat.maratori@gmail.com>
2022-08-28 22:32:41 +0300

    Init go module

---
+++ pkg/testableexamples/testpackage.go
@@ -0,0 +1,16 @@
+package testpackage
+
+import (
+	"golang.org/x/tools/go/analysis"
+)
+
+// NewAnalyzer returns Analyzer that checks if examples are testable.
+func NewAnalyzer() *analysis.Analyzer {
+	return &analysis.Analyzer{
+		Name: "testableexamples",
+		Doc:  "linter checks if examples are testable",
+		Run: func(pass *analysis.Pass) (interface{}, error) {
+			return nil, nil
+		},
+	}
+}
