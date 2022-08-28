8cb8162ac1a59b2c3924910ec31f11d0250b3ad2
Marat Reymers <marat.maratori@gmail.com>
2022-08-29 01:53:00 +0300

    Implement the linter

--- pkg/testableexamples/testableexamples.go
+++ pkg/testableexamples/testableexamples.go
@@ -1,6 +1,10 @@
 package testableexamples
 
 import (
+	"go/ast"
+	"go/doc"
+	"strings"
+
 	"golang.org/x/tools/go/analysis"
 )
 
@@ -8,8 +12,22 @@ import (
 func NewAnalyzer() *analysis.Analyzer {
 	return &analysis.Analyzer{
 		Name: "testableexamples",
-		Doc:  "linter checks if examples are testable",
+		Doc:  "linter checks if examples are testable (have an expected output)",
 		Run: func(pass *analysis.Pass) (interface{}, error) {
+			testFiles := make([]*ast.File, 0, len(pass.Files))
+			for _, file := range pass.Files {
+				fileName := pass.Fset.File(file.Pos()).Name()
+				if strings.HasSuffix(fileName, "_test.go") {
+					testFiles = append(testFiles, file)
+				}
+			}
+
+			for _, example := range doc.Examples(testFiles...) {
+				if example.Output == "" && !example.EmptyOutput {
+					pass.Reportf(example.Code.Pos(), "missing output for example, go test can't validate it")
+				}
+			}
+
 			return nil, nil
 		},
 	}
